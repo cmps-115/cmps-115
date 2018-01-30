f432be43e6438184e8efeb8dff3c8455388c7c16
zinhart <azdegrac@ucsc.edu>
2018-01-30 13:50:27 -0800

    Merge Akobir Branch Board and Pair scripts

---
+++ globals/move.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using ChessGlobals;
+
+public class Move
+{
+								public:
+																Move(Position src, Position des);
+																Position src;
+																Position des;
+
+
+}
---
+++ globals/pair.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Name: Akobir Khamidov
+ * 
+ * */
+
+public class Pair
+{
+    private Position pos;
+    private Piece pie;
+
+	public Pair(Position pos, Piece p)
+	{
+        this.pos = pos;
+        this.pie = pie;
+	}
+
+    public void SetPosition(Position pos)
+    {
+        this.pos = pos;
+    }
+
+    public void setPiece(Piece p)
+    {
+        this.pie = p;
+    }
+
+    public Position GetPosition()
+    {
+        return pos;
+    }
+
+    public Piece GetPiece()
+    {
+        return pie;
+    }
+}
