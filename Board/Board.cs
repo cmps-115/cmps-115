5bb4f8aa947cb6edf16bc99ff6ec07bc56f4d642
AKhamido <akobirkhamidov@gmail.com>
2018-01-29 19:14:15 -0800

    adding loop

--- Board/Board.cs
+++ Board/Board.cs
@@ -10,6 +10,11 @@ public class Board{
     public Board()
     {
         squares = new Pair[8, 8];
+
+        //initialze to null
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                sqaures[i][j] = null;
     }
 
     public void Mark(Postion square, Piece piece)
