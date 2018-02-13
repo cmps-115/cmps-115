1f64b1ee5efe3d31ae2b1e02e8b740ef2aac2442
zinhart <azdegrac@ucsc.edu>
2018-02-13 00:29:58 -0800

    Added Display for current players turn

---
+++ Chess/Assets/Scripts/game_scripts/globals/Move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class Move
+{
+	Vector2 src;
+	Vector2 des;
+	public Move ()
+	{
+	}
+	public Move (Vector2 src, Vector2 des)
+	{
+		this.src = src;
+		this.des = des;
+	}
+}
+ 
+
---
+++ Chess/Assets/Scripts/game_scripts/player_scripts/player.cs
@@ -0,0 +1,8 @@
+public abstract class Player{
+	public int id;
+	public char color;
+	abstract public Move getMove ();
+	abstract public bool moveSuccessfullyExcecuted ();
+	abstract public int getId ();
+	abstract public char getColor ();
+}
