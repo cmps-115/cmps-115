39e3450be36f96baccfafec55bbd909484bd09fe
AKhamido <akobirkhamidov@gmail.com>
2018-02-15 18:36:45 -0800

    getting project

---
+++ Chess/Assets/Scripts/Model Scripts/DrawPiece.cs
@@ -0,0 +1,172 @@
+﻿// Programmer: Ari Berkson
+//
+// This module places the chess peices on the board.
+// This module also detects when the user clicks on a chess peice model.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawPiece : MonoBehaviour {
+
+    public GameObject king;
+    public GameObject queen;
+    public GameObject bishop;
+    public GameObject knight;
+    public GameObject castle;
+    public GameObject pawn;
+
+    public Material firstTeam;
+    public Material secondTeam;
+
+    private static Vector2 piecePosition = new Vector2(-1, -1);
+    private static bool clicked = false;
+
+    private const float MODEL_OFFSET = 0.5f;
+    private const int BOARD_MINIMUM = 0;
+    private const int BOARD_MAXIMUM = 7;
+    private const int TEAM_ROWS = 2;
+
+    /// <summary>
+    /// Returns the positions of the last chess piece clicked.
+    /// </summary>
+    public static Vector2 PiecePosition
+    {
+        get { return piecePosition;}
+    }
+
+    public static bool IsClicked
+    {
+        
+        get
+        {
+            DetectClick();
+            return clicked;
+        }
+    }
+
+    // Use this for initialization
+    private void Start ()
+    {
+        InitPieces();
+    }
+
+    private void InitPieces()
+    {
+        PlaceFirstTeam();
+        PlaceSecondTeam();
+    }
+
+    #region Place Teams
+    private GameObject Spawn(int x, int y, GameObject model)
+    {
+        return Instantiate(model, new Vector3(x + MODEL_OFFSET, 0, y + MODEL_OFFSET), model.transform.rotation);
+    }
+
+    private void PlaceFirstTeam()
+    {
+        for (int y = BOARD_MINIMUM; y < TEAM_ROWS; ++y)
+        {
+            GameObject model = null;
+            for (int x = BOARD_MINIMUM; x <= BOARD_MAXIMUM; ++x)
+            {
+                if (y == 1)
+                {
+                    model = Spawn(x, y, pawn);
+                }
+                else
+                {
+                    if (x == 0 || x == 7)
+                    {
+                        model = Spawn(x, y, castle);
+                    }
+                    else if (x == 1 || x == 6)
+                    {
+                        model = Spawn(x, y, knight);
+                    }
+                    else if (x == 2 || x == 5)
+                    {
+                        model = Spawn(x, y, bishop);
+                    }
+                    else if (x == 3)
+                    {
+                        model = Spawn(x, y, king);
+                    }
+                    else if (x == 4)
+                    {
+                        model = Spawn(x, y, queen);
+                    }
+                }
+                model.GetComponent<MeshRenderer>().material = firstTeam;
+            }
+        }
+    }
+
+    private void PlaceSecondTeam()
+    {
+        for (int y = BOARD_MAXIMUM; y > BOARD_MAXIMUM - TEAM_ROWS; --y)
+        {
+            GameObject model = null;
+            for (int x = BOARD_MINIMUM; x <= 7; ++x)
+            {
+                if (y == 6)
+                {
+                    model = Spawn(x, y, pawn);
+                }
+                else
+                {
+                    if (x == 0 || x == 7)
+                    {
+                        model = Spawn(x, y, castle);
+                    }
+                    else if (x == 1 || x == 6)
+                    {
+                        model = Spawn(x, y, knight);
+                    }
+                    else if (x == 2 || x == 5)
+                    {
+                        model = Spawn(x, y, bishop);
+                    }
+                    else if (x == 3)
+                    {
+                        model = Spawn(x, y, king);
+                    }
+                    else if (x == 4)
+                    {
+                        model = Spawn(x, y, queen);
+                    }
+                }
+                model.GetComponent<MeshRenderer>().material = secondTeam;
+            }
+        }
+    }
+    #endregion
+
+
+    /// <summary>
+    /// Detects when a piece has been clicked on.
+    /// Should be called in an update function.
+    /// </summary>
+    private static void DetectClick()
+    {
+        //Checks for left mouse button down.
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            //create a ray from the camera through the mouse cursor.
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "ChessPiece")
+                {
+                    piecePosition = new Vector2Int((int)hit.transform.position.x, (int)hit.transform.position.z);
+                    clicked = true;
+                    return;
+                }
+            }
+        }
+        clicked = false;
+    }
+}
---
+++ Chess/Assets/Scripts/Model Scripts/MoveModel.cs
@@ -0,0 +1,127 @@
+﻿// Programmer: Ari Berkson
+//
+// This module moves 1 piece from one location to another.
+// Multiple instances of this module will be needed if more than piece needs to be moved at the same time.
+// This module will also delete a chess model if a piece is moved on top of it
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveModel : MonoBehaviour {
+
+    private Transform objToMove;
+    private GameObject otherObj;
+    private Vector3 originalPos;
+    private Vector3 midPoint;
+    private Vector3 finalPos;
+
+    private bool moving = false;
+    private float timeStarted = 0;
+
+    private float movementTime = 1;
+
+    private const float MODEL_OFFSET = 0.5f;
+    private const int BOARD_MINIMUM = 0;
+    private const int BOARD_MAXIMUM = 7;
+
+    /// <summary>
+    /// Moves a piece from (x1, y1) to (x2, y2).
+    /// The height determines the height of the arc (default = 2). Set height to zero to slide piece.
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="height"></param>
+    public void MovePiece(Vector2 from, Vector2 to, int height = 2)
+    {
+        if (height < 0) throw new System.Exception("Error in MoveModel: MovePiece() height parameter must be greater than zero.");
+        if (from.x < BOARD_MINIMUM || to.x < BOARD_MINIMUM || from.y > BOARD_MAXIMUM || to.y > BOARD_MAXIMUM) throw new System.Exception("Error in MoveModel: MovePiece() coordinates out of range.");
+
+        if (!moving)
+        {
+            CheckFrom(from, to, height);
+            CheckTo(to);
+        }
+    }
+
+    /// <summary>
+    /// Sets how long it takes to move the piece in seconds.
+    /// </summary>
+    public float MovementTime
+    {
+        set
+        {
+            if (value > 0) movementTime = value;
+            else throw new System.Exception("Error in MoveModel: movement time cannot be cannot be less than or equal to zero");
+        }
+        get { return movementTime; }
+    }
+
+    public bool isMoving
+    {
+        get { return moving; }
+    }
+
+    // Update is called once per frame
+    private void Update ()
+    {
+        if (objToMove != null)
+        {
+            if (moving)
+            {
+                var dTime = Time.time - timeStarted;
+                var l1 = Vector3.Lerp(originalPos, midPoint, dTime / movementTime);
+                var l2 = Vector3.Lerp(midPoint, finalPos, dTime / movementTime);
+                var l3 = Vector3.Lerp(l1, l2, dTime / movementTime);
+                objToMove.position = l3;
+            }
+            if (objToMove.position == finalPos)
+            {
+                if (otherObj != null)
+                {
+                    if (otherObj.transform.position == objToMove.position) Destroy(otherObj);
+                }
+                moving = false;
+            }
+        }
+    }
+
+    private Collider[] GetOverLappingColliders(Vector3 position)
+    {
+        return Physics.OverlapSphere(new Vector3(position.x + MODEL_OFFSET, MODEL_OFFSET, position.y + MODEL_OFFSET), MODEL_OFFSET);
+    }
+
+    private void CheckFrom(Vector2 from, Vector2 to, int height = 2)
+    {
+        Collider[] cols = GetOverLappingColliders(from);
+        if (cols.Length > 0)
+        {
+            foreach (Collider c in cols)
+            {
+                if (c.transform.tag == "ChessPiece")
+                {
+                    objToMove = c.transform;
+                    originalPos = objToMove.position;
+                    midPoint = (new Vector3(to.x + MODEL_OFFSET, height * 2, to.y + MODEL_OFFSET) + originalPos) / 2;
+                    finalPos = new Vector3(to.x + MODEL_OFFSET, originalPos.y, to.y + MODEL_OFFSET);
+                    moving = true;
+                    timeStarted = Time.time;
+                    return;
+                }
+            }
+        }
+        else throw new System.Exception("Error in MoveModel: MovePiece() could not find chess model at location.");
+    }
+
+    private void CheckTo(Vector2 to)
+    {
+        Collider[] cols =  GetOverLappingColliders(to);
+        if (cols.Length > 0)
+        {
+            foreach (Collider c in cols)
+            {
+                if (c.transform.tag == "ChessPiece") otherObj = c.gameObject;
+            }
+        }
+    }
+}
