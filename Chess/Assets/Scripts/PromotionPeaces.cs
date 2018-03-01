7a0a3d4b6329c83ad23bf005945710533073282c
AKhamido <akobirkhamidov@gmail.com>
2018-03-01 08:06:07 -0800

    added promotion menu

---
+++ Chess/Assets/Scripts/PromotionPeaces.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromotionPeaces : MonoBehaviour {
+
+    public void clickedButton(string name)
+    {
+        Debug.Log(name + " button was clicked");
+    }
+}
