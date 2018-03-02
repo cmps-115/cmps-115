d6b7428097fb90deac2c07bff66a5b5836c8a803
AKhamido <akobirkhamidov@gmail.com>
2018-03-02 08:43:11 -0800

    UI promotion

---
+++ Chess/Assets/Scripts/Test Scripts/PromotionTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromotionTest : MonoBehaviour {
+
+    public GameObject PromoteMenu;
+    public GameObject PromoteButton;
+    public void promote()
+    {
+        pauseGame();
+    }
+
+    public void button_clicked(string name)
+    {
+        Debug.Log(name + " button was clicked");
+        resumeGame();
+    }
+
+    public void pauseGame()
+    {
+        Time.timeScale = 0f;
+        PromoteMenu.SetActive(true);
+        PromoteButton.SetActive(false);
+        Input.GetKeyDown(KeyCode.Escape);
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1f;
+        PromoteMenu.SetActive(false);
+        PromoteButton.SetActive(true);
+    }
+}
